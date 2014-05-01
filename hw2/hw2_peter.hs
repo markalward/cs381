77bf80cbfad55f5545a3f0fcdd36a51ec70ca4aa
Brennan Kucey <kuceyb@flip1.engr.oregonstate.edu>
2014-04-30 21:59:13 -0700

    changed CmdX, StateX, and ProgX for each of the different exercises X

--- hw2/hw2_peter.hs
+++ hw2/hw2_peter.hs
@@ -6,9 +6,40 @@
 -- ex 1                                                           --
 --------------------------------------------------------------------
 
--- this is also 2a
-type Prog = [Cmd]
-data Cmd = LD Int
+type Prog = [Cmd1]
+data Cmd1 = LD Int
+         | ADD
+         | MULT
+         | DUP
+         deriving Show
+
+type Stack = [Int]
+
+-- D is the semantic domain
+type D = Maybe Stack1 -> Maybe Stack1
+
+semCmd1 :: Cmd1 -> D1
+semCmd1 _ Nothing = Nothing
+semCmd1 (LD i) (Just st) = Just (i : st)
+semCmd1 ADD (Just st) = case st of (x:y:xs) -> Just ((x+y) : xs)
+                                  _ -> Nothing
+semCmd1 MULT (Just st) = case st of (x:y:xs) -> Just ((x*y) : xs)
+                                   _ -> Nothing
+semCmd1 DUP (Just st) = case st of (x:xs) -> Just (x:x:xs)
+                                  _ -> Nothing
+
+sem1 :: Prog1 -> D1
+sem1 _ Nothing = Nothing
+sem1 [] st = st
+sem1 (x:xs) st = case semCmd1 x st of Nothing -> Nothing
+                                    newState -> sem xs newState
+
+--------------------------------------------------------------------
+-- ex 2.a                                                         --
+--------------------------------------------------------------------
+
+type Prog2 = [Cmd2]
+data Cmd2 = LD Int
          | ADD
          | MULT
          | DUP
@@ -16,32 +47,26 @@ data Cmd = LD Int
          | CALL String
          deriving Show
 
-type Stack = [Int]
+--Defined in ex 1
+	--type Stack = [Int]
+	-- D is the semantic domain
+	--type D = Maybe Stack -> Maybe Stack
 
--- D is the semantic domain
-type D = Maybe Stack -> Maybe Stack
-
-semCmd :: Cmd -> D
-semCmd _ Nothing = Nothing
-semCmd (LD i) (Just st) = Just (i : st)
-semCmd ADD (Just st) = case st of (x:y:xs) -> Just ((x+y) : xs)
+semCmd2 :: Cmd2 -> D
+semCmd2 _ Nothing = Nothing
+semCmd2 (LD i) (Just st) = Just (i : st)
+semCmd2 ADD (Just st) = case st of (x:y:xs) -> Just ((x+y) : xs)
                                   _ -> Nothing
-semCmd MULT (Just st) = case st of (x:y:xs) -> Just ((x*y) : xs)
+semCmd2 MULT (Just st) = case st of (x:y:xs) -> Just ((x*y) : xs)
                                    _ -> Nothing
-semCmd DUP (Just st) = case st of (x:xs) -> Just (x:x:xs)
+semCmd2 DUP (Just st) = case st of (x:xs) -> Just (x:x:xs)
                                   _ -> Nothing
 
-sem :: Prog -> D
-sem _ Nothing = Nothing
-sem [] st = st
-sem (x:xs) st = case semCmd x st of Nothing -> Nothing
+sem1 :: Prog1 -> D
+sem1 _ Nothing = Nothing
+sem1 [] st = st
+sem1 (x:xs) st = case semCmd2 x st of Nothing -> Nothing
                                     newState -> sem xs newState
-
---------------------------------------------------------------------
--- ex 2.a                                                         --
---------------------------------------------------------------------
-
--- see ex 1
 
 --------------------------------------------------------------------
 -- ex 2.b                                                        --
@@ -49,13 +74,13 @@ sem (x:xs) st = case semCmd x st of Nothing -> Nothing
 
 type Macros = [(String,Prog)]
 
-data State = S Stack Macros | Error deriving Show
+data State2 = S Stack Macros | Error deriving Show
 
 --------------------------------------------------------------------
 -- ex 2.c                                                        --
 --------------------------------------------------------------------
 
-semCmd2 :: Cmd -> State -> State
+semCmd2 :: Cmd2 -> State2 -> State2
 semCmd2 _ Error = Error
 semCmd2 (DEF name p) (S st macros) = S st ((name, p) : macros)
 semCmd2 (CALL name) s@(S st macros) = case lookup name macros of
@@ -70,7 +95,7 @@ semCmd2 DUP (S st macros) = case st of (x:xs) -> S (x:x:xs) macros
                                        _ -> Error
 
 
-sem2 :: Prog -> State -> State
+sem2 :: Prog -> State2 -> State2
 sem2 _ Error = Error
 sem2 [] st = st
 sem2 (x:xs) st = case semCmd2 x st of Error -> Error
@@ -80,17 +105,17 @@ sem2 (x:xs) st = case semCmd2 x st of Error -> Error
 -- ex 3  														  --
 --------------------------------------------------------------------
 
-data Cmd = Pen Mode
+data Cmd3 = Pen Mode
          | MoveTo Int Int
          | Seq Op Op
 
 data Mode = Up | Down
 
-type State = (Mode,Int,Int)
+type State3 = (Mode,Int,Int)
 type Line = (Int,Int,Int,Int)
 type Lines = [Line]
 
-semS :: Cmd -> State -> (State,Lines)
+semS :: Cmd3 -> State3 -> (State3,Lines)
 semS (Pen    m    ) (_,   x,y) = ((m   ,x ,y ), [           ]        )
 semS (MoveTo x' y') (Down,x,y) = ((Down,x',y'), [(x,y,x',y')]        )
 semS (MoveTo x' y') (Up,  _,_) = ((Up  ,x',y'), [           ]        )
@@ -99,7 +124,7 @@ semS (Seq    s1 s2)  st        = ( fst r2     , (snd r1) ++ (snd r2) )
                         r1 = semS s1 st
                         r2 = semS s2 (fst r1)
 
-sem' :: Cmd -> Lines
+sem' :: Cmd3 -> Lines
 sem' c = snd (semS c (Up,0,0)) 
 
 
